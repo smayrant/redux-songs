ea488a52b0e1a7b742d81a87e1358af7b98109c3
Sheldrick <mrmayrant@yahoo.com>
2019-08-10 06:56:21 -0400

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+	return [
+		{ title: "Hey now!", duration: "4.:05" },
+		{ title: "All star", duration: "3.:45" },
+		{ title: "I want it that way", duration: "2.:42" },
+		{ title: "Emotions", duration: "3.:41" }
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === "SONG_SELECTED") {
+		return action.payload;
+	}
+
+	return selectedSong;
+};
